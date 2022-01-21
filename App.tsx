d37c0cabd386d1075b29ac53eb5c8dcef4a31227
Mouhamadou Ndour <taphacr@gmail.com>
2022-01-21 14:21:27 +0100

    #2 Modification du style

--- App.tsx
+++ App.tsx
@@ -1,7 +1,5 @@
-import { StatusBar } from "expo-status-bar"
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
-import { StyleSheet, Text, View } from 'react-native'
 import { Connection, Home } from "./pages"
 
 const Stack = createNativeStackNavigator()
@@ -10,19 +8,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
         <Stack.Screen name="Connection" component={Connection} />
       </Stack.Navigator>
-      <StatusBar style="light" backgroundColor="#000000" />
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- pages/Home/index.tsx
+++ pages/Home/index.tsx
@@ -1,41 +1,107 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native"
+import { StatusBar } from "expo-status-bar"
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native"
+import AppLoading from 'expo-app-loading'
+import { useFonts, Pacifico_400Regular } from '@expo-google-fonts/pacifico';
 
 const Home = ({ navigation }: any) => {
 
-    return (
-        <View style={styles.container}>
-            <Text>Open up App.tsx to start working on your app!</Text>
-            <TouchableOpacity
-                onPress={() => navigation.navigate("Connection")}
-                style={styles.btn}
-            >
-                <Text style={styles.btnText}>Ouvrir Connexion</Text>
-            </TouchableOpacity>
-        </View>
-    )
+    let [fontsLoaded] = useFonts({
+        Pacifico_400Regular,
+    });
+
+    if (!fontsLoaded) {
+        return <AppLoading />
+    }
+    else {
+        return (
+            <View style={styles.container}>
+                <View style={styles.brandContainer}>
+                    <Image
+                        style={styles.image}
+                        resizeMode="cover"
+                        source={require('./../../assets/icon_white.png')}
+                    />
+                    <Text style={styles.brand}>Moure</Text>
+                    <Text style={styles.subbrand}>Bienvenue sur Moure !</Text>
+                    <Text style={styles.subbrand}>Partagez vos plus beaux souvenirs avec vos amis</Text>
+                </View>
+                <View style={styles.buttonsContainer}>
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate("Connection")}
+                        style={styles.btnLeft}
+                    >
+                        <Text style={styles.btnText}>Connexion</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate("Inscription")}
+                        style={styles.btnRight}
+                    >
+                        <Text style={styles.btnText}>Inscription</Text>
+                    </TouchableOpacity>
+                </View>
+                <StatusBar style="light" />
+            </View>
+        )
+    }
+
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#fff",
+        backgroundColor: "#2f007c",
         alignItems: "center",
         justifyContent: "center",
     },
-    img: {
-        width: 305,
-        height: 159,
+    brandContainer: {
+        flex: 2,
+        width: '100%',
+        alignItems: "center",
+        justifyContent: "flex-end",
+        paddingBottom: 100,
     },
-    btn: {
-        marginRight: 40,
-        marginLeft: 40,
-        marginTop: 10,
-        padding: 20,
-        backgroundColor: "#68a0cf",
-        borderRadius: 10,
+    buttonsContainer: {
+        flex: 1,
+        flexDirection: "row",
+        width: '100%',
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    brand: {
+        color: "#fff",
+        fontSize: 35,
+        fontWeight: "bold",
+        fontFamily: 'Pacifico_400Regular',
+        marginBottom: 10,
+    },
+    subbrand: {
+        color: "#a67efa",
+    },
+    image: {
+        width: 80,
+        height: 60,
+    },
+    btnLeft: {
+        marginRight: 1,
+        padding: 15,
+        flex: 1,
+        backgroundColor: "#D9205C",
+        alignItems: 'center',
+        width: '50%',
+    },
+    btnRight: {
+        marginLeft: 1,
+        padding: 15,
+        backgroundColor: "#f79f1a",
+        flex: 1,
+        alignItems: 'center',
+        width: '50%',
     },
     btnText: {
         color: "#fff",
+        fontWeight: 'bold',
+        fontFamily: 'Pacifico_400Regular',
+        fontSize: 20,
     },
 })
 

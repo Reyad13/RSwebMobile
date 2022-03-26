c550dc1105a794f3d277301649ce182994fdb8ab
Mouhamadou Ndour <taphacr@gmail.com>
2022-03-26 16:10:50 +0100

    #11 Création d'une nouvelle publication

--- pages/New/index.tsx
+++ pages/New/index.tsx
@@ -1,24 +1,57 @@
 import { useTheme } from "@react-navigation/native"
-import { StyleSheet, Text } from "react-native"
+import { Button, StyleSheet, Image, TextInput, View } from "react-native"
+import { Formik } from 'formik'
+import * as ImagePicker from 'expo-image-picker'
+import { useState } from "react"
 
 const New = () => {
 
     const { colors } = useTheme()
+    const [image, setImage] = useState(null)
+
+    const pickImage = async () => {
+        // No permissions request is necessary for launching the image library
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        })
+
+        //console.log(result)
+
+        if (!result.cancelled) {
+            setImage(result.uri)
+        }
+    }
 
     return (
-        <Text style={[styles.brand, { color: colors.text }]}>New</Text>
+        <Formik
+            initialValues={{ caption: '', image: "" }}
+            onSubmit={values => console.log(values)}
+        >
+            {({ handleChange, handleBlur, handleSubmit, values }) => (
+                <View>
+                    <Button title="Nouvelle publication" onPress={pickImage} />
+                    {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+                    <TextInput
+                        onChangeText={handleChange('caption')}
+                        onBlur={handleBlur('caption')}
+                        value={values.caption}
+                        style={styles.captionInput}
+                    />
+                    <Button onPress={handleSubmit} title="Submit" />
+                </View>
+            )}
+        </Formik>
     )
 }
 
 const styles = StyleSheet.create({
-    brand: {
-        fontSize: 26,
-        fontWeight: "bold",
-        fontFamily: 'Pacifico_400Regular',
-        flex: 0,
-        alignItems: 'flex-start',
-        width: '100%'
-    },
+    captionInput: {
+        backgroundColor: 'red',
+        marginTop: 20
+    }
 })
 
 export default New
